e04a1117c7f5431f5ada374ccb70426e57a03855
DevVermaJTG <dev.verma@joshtechnologygroup.com>
2026-06-20 10:35:57 +0530

    wrapped hamburger icon inside button for accessibility through keyboard, added functionality of closing dropdown when user clicked outside dropdown

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,15 @@
 const hamburger=document.querySelector('#hamburger');
 const navMenu=document.querySelector('.nav-menu');
+
 hamburger.addEventListener('click',()=>{
     navMenu.classList.toggle('show');
-})
+});
+
+document.addEventListener('click', (event) => {
+    // If menu is open AND click is NOT inside menu AND click is NOT on hamburger
+    if (navMenu.classList.contains('show') && 
+        !navMenu.contains(event.target) && 
+        !hamburger.contains(event.target)) {
+      navMenu.classList.remove('show');
+    }
+  });
