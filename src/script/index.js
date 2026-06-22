156806dfd89ef44343a3781e6ca3e27d60923ddc
DevVermaJTG <dev.verma@joshtechnologygroup.com>
2026-06-22 22:14:39 +0530

    [DV_A1_01]: fixed the ui of nav links when hamburger is clicked , js logic of trapped the focus inside the nav links when modal is open

--- src/script/index.js
+++ src/script/index.js
@@ -1,22 +1,47 @@
 const hamburger=document.querySelector('#hamburger');
 const navMenu=document.querySelector('.nav-menu');
+const hideMenuBtn=document.querySelector('.hide-menu-btn');
+const isMobileQuery = window.matchMedia("(max-width: 430px)");
+
+function getIsMobile() {
+    return isMobileQuery.matches;
+}
 
 hamburger.addEventListener('click',()=>{
     navMenu.classList.toggle('show');
 });
 
-document.addEventListener('click', (event) => {
-    // If menu is open AND click is NOT inside menu AND click is NOT on hamburger
-    if (navMenu.classList.contains('show') && 
-        !navMenu.contains(event.target) && 
-        !hamburger.contains(event.target)) {
-      navMenu.classList.remove('show');
-    }
+hideMenuBtn.addEventListener('click',()=>{
+        navMenu.classList.remove('show');
+        return;
 });
 
 document.addEventListener('keydown', (event) => {
     // Close on Escape
     if (event.key === 'Escape' && navMenu.classList.contains('show')) {
         navMenu.classList.remove('show');
+        return;
     }
+    if(event.key==='Tab' && navMenu.classList.contains('show')){
+        const current=document.activeElement;
+        const isMobile = getIsMobile();
+        if(isMobile){
+            if (current.classList.contains('last-hamburger-nav-for-mobile')) {
+                event.preventDefault();
+                hideMenuBtn.focus();
+            }
+        }
+        else if (current.classList.contains('last-hamburger-nav-for-tablet')) {
+            event.preventDefault();
+            hideMenuBtn.focus();
+        }
+    }
+});
+hamburger.addEventListener('click',()=>{
+    if(navMenu.classList.contains('show')){
+        document.body.style.overflow='hidden';
+    }
+});
+hideMenuBtn.addEventListener('click',()=>{
+    document.body.style.overflow='visible';
 });
