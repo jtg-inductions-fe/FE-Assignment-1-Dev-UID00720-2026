962deb8af39ac90c5684a2a0356840f7caceeb5e
DevVermaJTG <dev.verma@joshtechnologygroup.com>
2026-07-03 22:46:58 +0530

    [DV_A1_06]: Fixed the code which was causing issue on production

--- src/script/index.js
+++ src/script/index.js
@@ -207,7 +207,7 @@ function renderUnlockedDeals(dealsWon) {
                                 </div>
                                 <img
                                     class="deal-details__code-section__copy-btn"
-                                    src="public/assets/copy.svg"
+                                    src="/assets/copy.svg"
                                     alt="copy coupon code"
                                 />
                             </div>
