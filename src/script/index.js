508191101dde8a533cfab19695549e86f263b74d
DevVermaJTG <dev.verma@joshtechnologygroup.com>
2026-07-03 12:56:01 +0530

    [DV_A1_06]: Added js logic for calling the api

--- src/script/index.js
+++ src/script/index.js
@@ -89,196 +89,217 @@ function openFooterLinks() {
 openFooterLinks();
 
 // --------------------------------------------------------------logics for spin wheel modal-------------------------------------------------------------
-const specialDealsOpenBtn = document.querySelector('.special-deals-btn');
-const specialDealsModal = document.querySelector('.deals-modal');
-const specialDealsCloseBtn = document.querySelector(
-    '.deals-modal__special-deals__close-btn',
-);
-const spinBtn = document.querySelector('.spin-wheel__circle__spin-btn');
+const apiUrl =
+    'https://gist.githubusercontent.com/ameer-wajid-ali/1f29ebee4295cede36f8d74b45e576df/raw/122966c9a123861249f173911d8d93a76dc06d7a/';
+let apiCalled = false;
+let dealsData = [];
+let dealsWon = [];
+let initialDealsLoaded = true; // this will be become false after the wheel has spinned one time
+let alreadySpin = 0; // to how much degree has the wheel already spined
 
-specialDealsOpenBtn.addEventListener('click', () => {
+const displayDealWon = document.querySelector('.deal-won');
+const specialDealsOpenBtn = document.querySelector('.special-deals-btn');
+const dealsModal = document.querySelector('.deals-modal');
+const dealsCloseBtns = document.querySelectorAll('.deals__close-btn');
+const spinBtn = document.querySelector('.spin-wheel__circle__spin-btn');
+const viewUnlockDealsBtn = document.querySelector('.view-unlock-deals');
+const specialDealsSection = document.querySelector('.special-deals');
+const unlockedDealsSection = document.querySelector('.unlocked-deals');
+const backToSpecialDealsBtn = document.querySelector(
+    '.back-to-special-deals-btn',
+);
+
+specialDealsOpenBtn.addEventListener('click', async () => {
     setMenuOpen(false);
     document.body.style.overflow = 'hidden';
-    specialDealsModal.style.display = 'block';
+    dealsModal.style.display = 'block';
+
+    if (!apiCalled) {
+        const rawData = await fetch(apiUrl);
+        const data = await rawData.json();
+        dealsData = structuredClone(data);
+        apiCalled = true;
+        document.querySelector('.spin-wheel').style.display = 'block';
+        document.querySelector('.loading-wheel').style.display = 'none';
+        document.querySelector('.view-unlock-deals').style.display = 'flex';
+        init();
+    }
 });
 
-specialDealsCloseBtn.addEventListener('click', () => {
-    document.body.style.overflow = '';
-    specialDealsModal.style.display = 'none';
-});
+function init() {
+    dealsCloseBtns.forEach((dealsCloseBtn) => {
+        dealsCloseBtn.addEventListener('click', () => {
+            document.body.style.overflow = '';
+            dealsModal.style.display = 'none';
+        });
+    });
+    viewUnlockDealsBtn.addEventListener('click', () => {
+        specialDealsSection.style.display = 'none';
+        unlockedDealsSection.style.display = 'block';
+    });
+    backToSpecialDealsBtn.addEventListener('click', () => {
+        unlockedDealsSection.style.display = 'none';
+        specialDealsSection.style.display = 'block';
+    });
 
-let dealsData = [
-    {
-        label: '20% Off Flights',
-        promoCode: 'FLY20-X8J2',
-        validFor: 13,
-    },
-    {
-        label: 'Free Hotel Night',
-        promoCode: 'HOTEL-9A4B',
-        validFor: null,
-    },
-    {
-        label: '10% Off Packages',
-        promoCode: 'PKG10-QW12',
-        validFor: 4,
-    },
-    {
-        label: 'VIP Lounge',
-        promoCode: 'VIP-LMN9',
-        validFor: 29,
-    },
-    {
-        label: 'Free Breakfast',
-        promoCode: 'BFAST-5541',
-        validFor: 1,
-    },
-    {
-        label: 'Free Airport Cab',
-        promoCode: 'CAB-XYZ9',
-        validFor: 6,
-    },
-    {
-        label: '15% Off Car Rentals',
-        promoCode: 'CAR15-JKL3',
-        validFor: 14,
-    },
-    {
-        label: 'Buy 1 Get 1 City Tour',
-        promoCode: 'BOGO-TR44',
-        validFor: 10,
-    },
-    {
-        label: 'Complimentary Spa Session',
-        promoCode: 'SPA-ZXC8',
-        validFor: 21,
-    },
-    {
-        label: 'Priority Boarding',
-        promoCode: 'PRI-BOARD1',
-        validFor: null,
-    },
-];
-const displayDealWon = document.querySelector('.deal-won');
-/**
- * Selects a random deal from the dealsData array and removes it.
- * @returns {object|null} The randomly selected deal object, or null if the array is empty.
- */
-function getRandomDeal() {
-    let size = dealsData.length;
-    if (size == 0) return null;
-    let pickedInd = Math.floor(Math.random() * size);
-    let deal = dealsData[pickedInd];
-    dealsData.splice(pickedInd, 1); // delete object at pickedInd
-    return deal;
-}
-
-/**
- * loads 4 random deals into the wheel , if there is no deal available it loads "No deal available"
- * @returns {Array of objects} array of deals that are randomly selected
- */
-function loadDealsInWheel() {
-    let dealsLoaded = [];
-    for (let i = 1; i <= 4; i++) {
-        let dealObj = getRandomDeal();
-        dealsLoaded.push(dealObj);
-        let wheelPrizeQuad = document
-            .querySelector(`.prize${i}`)
-            .querySelector('p');
-        wheelPrizeQuad.textContent = dealObj
-            ? dealObj.label
-            : 'No deal available';
+    /**
+     * Selects a random deal from the dealsData array and removes it.
+     * @returns {object|null} The randomly selected deal object, or null if the array is empty.
+     */
+    function getRandomDeal() {
+        let size = dealsData.length;
+        if (size == 0) return null;
+        let pickedInd = Math.floor(Math.random() * size);
+        let deal = dealsData[pickedInd];
+        dealsData.splice(pickedInd, 1); // delete object at pickedInd
+        return deal;
     }
-    return dealsLoaded;
-}
 
-//this is the initial loaded deals when the component render
-let dealsLoaded = loadDealsInWheel(); // deals that are already loaded in the wheel and removed from the dealsData
-let initialDealsLoaded = true; // this will be become false after the wheel has spinned one time
-let dealsWon = [];
-
-let alreadySpin = 0; // to how much degree has the wheel already spined
-// Function for spinning the wheel and calculating the reward which the user will get.
-function spinWheel() {
-    displayDealWon.style.display = 'none';
-    if (initialDealsLoaded) {
-        initialDealsLoaded = false; // if the deals are already fresh in the wheel then no need to load the deals again
-    } else {
-        dealsLoaded = loadDealsInWheel(); // if the wheel spins again this will load the fresh deals in the wheel again
+    /**
+     * loads 4 random deals into the wheel , if there is no deal available it loads "No deal available"
+     * @returns {Array of objects} array of deals that are randomly selected
+     */
+    function loadDealsInWheel() {
+        let dealsLoaded = [];
+        for (let i = 1; i <= 4; i++) {
+            let dealObj = getRandomDeal();
+            dealsLoaded.push(dealObj);
+            let wheelPrizeQuad = document
+                .querySelector(`.prize${i}`)
+                .querySelector('p');
+            wheelPrizeQuad.textContent = dealObj
+                ? dealObj.label
+                : 'No deal available';
+        }
+        return dealsLoaded;
     }
-    const wheel = document.querySelector('.spin-wheel__circle');
-    wheel.style.transition = '';
-    wheel.style.transform = `rotate(${alreadySpin}deg)`;
-    alreadySpin += 360 * 8; // Base rotations to ensure it spins more
-    const randomDeg = Math.floor(Math.random() * (350 - 10 + 1)) + 10;
 
-    const finalRotation = alreadySpin + randomDeg;
-    wheel.style.transition = 'transform 5s ease-out';
-    wheel.style.transform = `rotate(${finalRotation}deg)`;
-    spinBtn.disabled = true;
+    function renderUnlockedDeals(dealsWon) {
+        const displaySection = document.querySelector(
+            '.unlocked-deals__display-section',
+        );
+        displaySection.innerHTML = '';
+        dealsWon.forEach((dealObj) => {
+            let validFor = dealObj.validFor
+                ? `Expires in ${dealObj.validFor}d`
+                : 'Expires in 7d';
+            let dealDeatilsElement = `<div class="deal-details">
+                                <div class="deal-details__coupon-detail">
+                                    <div class="deal-details__coupon-detail__name">
+                                        ${dealObj.label}
+                                    </div>
+                                    <div
+                                        class="deal-details__coupon-detail__expired-in"
+                                    >
+                                        ${validFor}
+                                    </div>
+                                </div>
+                                <div class="deal-details__code-section">
+                                    <div
+                                        class="deal-details__code-section__coupon-code"
+                                    >
+                                        ${dealObj.promoCode}
+                                    </div>
+                                    <img
+                                        class="deal-details__code-section__copy-btn"
+                                        src="public/assets/Copy.svg"
+                                        alt="copy coupon code"
+                                    />
+                                </div>
+                            </div>`;
+            displaySection.innerHTML += dealDeatilsElement;
+            // let copyBtn = dealDeatilsElement.querySelector('.deal-details__copy-section__copy-btn');
+            // copyBtn.addEventListener('click', handleClick);
+            initCouponCopy();
+        });
+    }
 
-    const quad = Math.floor((finalRotation % 360) / 90) + 1;
-    let dealWonLabel = document
-        .querySelector(`.prize${quad}`)
-        .querySelector('p').textContent; // Label of the deal that we won
-    let dealWon = null; // dealWON object
-    dealsLoaded.forEach((dealObj) => {
-        if (dealObj == null) {
-            // No deal available
-        } else if (dealObj.label === dealWonLabel) {
-            dealWon = dealObj;
-            dealsWon.push(dealObj); // deal that user won on spinning the wheel push that to dealsWon array
+    //this is the initial loaded deals when the component render
+    let dealsLoaded = loadDealsInWheel(); // deals that are already loaded in the wheel and removed from the dealsData
+
+    // Function for spinning the wheel and calculating the reward which the user will get.
+    function spinWheel() {
+        displayDealWon.style.display = 'none';
+        if (initialDealsLoaded) {
+            initialDealsLoaded = false; // if the deals are already fresh in the wheel then no need to load the deals again
         } else {
-            dealsData.push(dealObj); // deals that have not won yet but still are loaded in wheel push them back to the deals data to unsure randomness
+            dealsLoaded = loadDealsInWheel(); // if the wheel spins again this will load the fresh deals in the wheel again
         }
-    });
-    dealsLoaded = []; // empty the deals that are loaded in wheel
+        const wheel = document.querySelector('.spin-wheel__circle');
+        wheel.style.transition = '';
+        wheel.style.transform = `rotate(${alreadySpin}deg)`;
+        alreadySpin += 360 * 8; // Base rotations to ensure it spins more
+        const randomDeg = Math.floor(Math.random() * (350 - 10 + 1)) + 10;
 
-    setTimeout(() => {
-        spinBtn.disabled = false;
-        displayDealWon.style.display = 'block';
+        const finalRotation = alreadySpin + randomDeg;
+        wheel.style.transition = 'transform 5s ease-out';
+        wheel.style.transform = `rotate(${finalRotation}deg)`;
+        spinBtn.disabled = true;
 
-        // logic to show the details of the deal won to user
-        displayDealWon.querySelector(
-            '.deal-details__coupon-detail__name',
-        ).textContent = dealWon.label;
-        if (dealWon.validFor) {
+        const quad = Math.floor((finalRotation % 360) / 90) + 1;
+        let dealWonLabel = document
+            .querySelector(`.prize${quad}`)
+            .querySelector('p').textContent; // Label of the deal that we won
+        let dealWon = null; // dealWON object
+        dealsLoaded.forEach((dealObj) => {
+            if (dealObj == null) {
+                // No deal available
+            } else if (dealObj.label === dealWonLabel) {
+                dealWon = dealObj;
+                dealsWon.push(dealObj); // deal that user won on spinning the wheel push that to dealsWon array
+            } else {
+                dealsData.push(dealObj); // deals that have not won yet but still are loaded in wheel push them back to the deals data to unsure randomness
+            }
+        });
+        dealsLoaded = []; // empty the deals that are loaded in wheel
+
+        setTimeout(() => {
+            spinBtn.disabled = false;
+            displayDealWon.style.display = 'block';
+
+            // logic to show the details of the deal won to user
             displayDealWon.querySelector(
-                '.deal-details__coupon-detail__expired-in',
-            ).textContent = `Expires in ${dealWon.validFor}d`;
-        }
-        displayDealWon.querySelector(
-            '.deal-details__code-section__coupon-code',
-        ).textContent = dealWon.promoCode;
+                '.deal-details__coupon-detail__name',
+            ).textContent = dealWon.label;
+            if (dealWon.validFor) {
+                displayDealWon.querySelector(
+                    '.deal-details__coupon-detail__expired-in',
+                ).textContent = `Expires in ${dealWon.validFor}d`;
+            }
+            displayDealWon.querySelector(
+                '.deal-details__code-section__coupon-code',
+            ).textContent = dealWon.promoCode;
 
-        // logic to display the total number of deals won in "view all unlocked deals" section
-        document.querySelector('.view-unlock-deals__count').textContent =
-            dealsWon.length;
-    }, 5000);
-}
-spinBtn.addEventListener('click', spinWheel);
-
-// function for coupon code component for coping the coupon code into the clipboard after clicking on copy button
-const handleClick = (e) => {
-    copyCouponFunction(e.currentTarget);
-};
-function copyCouponFunction(btn) {
-    // Target the immediate sibling element before the button
-    const codeElement = btn.previousElementSibling;
-
-    if (codeElement) {
-        const code = codeElement.innerText || codeElement.textContent;
-        navigator.clipboard.writeText(code);
+            // logic to display the total number of deals won in "view all unlocked deals" section
+            document.querySelector('.deals-navigation-btn__count').textContent =
+                dealsWon.length;
+            renderUnlockedDeals(dealsWon);
+        }, 5000);
     }
-}
-function initCouponCopy() {
-    // Select all copy buttons with the specific class
-    const copyButtons = document.querySelectorAll(
-        '.deal-details__code-section__copy-btn',
-    );
+    spinBtn.addEventListener('click', spinWheel);
 
-    copyButtons.forEach((btn) => {
-        btn.addEventListener('click', handleClick);
-    });
+    // function for coupon code component for coping the coupon code into the clipboard after clicking on copy button
+    const handleClick = (e) => {
+        copyCouponFunction(e.currentTarget);
+    };
+    function copyCouponFunction(btn) {
+        // Target the immediate sibling element before the button
+        const codeElement = btn.previousElementSibling;
+
+        if (codeElement) {
+            const code = codeElement.textContent;
+            navigator.clipboard.writeText(code);
+        }
+    }
+    function initCouponCopy() {
+        // Select all copy buttons with the specific class
+        const copyButtons = document.querySelectorAll(
+            '.deal-details__code-section__copy-btn',
+        );
+
+        copyButtons.forEach((btn) => {
+            btn.addEventListener('click', handleClick);
+        });
+    }
+    initCouponCopy();
 }
-initCouponCopy();
